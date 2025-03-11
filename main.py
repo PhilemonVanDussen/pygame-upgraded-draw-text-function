42b9df003a355fa38b39d46bd02b75f401037450
PhilemonVanDussen <vandussenph709@northwested.org>
2025-03-11 14:28:38 -0400

    yes

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import config # Import the config module
 
 def init_game():
     pygame.init()
+    pygame.font.init
     screen = pygame.display.set_mode((config.WINDOW_WIDTH, config.WINDOW_HEIGHT)) # Use constants from config
     pygame.display.set_caption(config.TITLE)
     return screen
@@ -19,13 +20,45 @@ def handle_events():
                 return False
     return True
 
+def draw_text(screen, text, font_size, font_name, color, position, italic=False, bold=False):
+    if font_name:
+        font = pygame.font.Font(font_name, font_size)
+    else:
+        font = pygame.font.Font(None, font_size)
+    
+    font.set_bold(bold)
+    font.set_italic(italic)
+
+    text_surface = font.render(text, True, color)
+    screen.blit(text_surface, (position))
+
+
 def main():
     screen = init_game()
     clock = pygame.time.Clock() # Initalize the clock here
+
+    font_name = "c:\Font\EmblemaOne-Regular.ttf"
+    font_name2 = "c:\Font\PlaywriteHU-VariableFont_wght.ttf"
+    font_name3 = "c:\Font\BigShouldersInline-VariableFont_opsz,wght.ttf"
+    font_color = config.PINK
+    font_color2 = config.ORANGE
+    font_color3 = config.PURPLE
+
+    text_pos = [300, 100]
+    text_pos2 = [150, 230]
+    text_pos3 = [400, 400]
+
+    
     running = True
     while running:
         running = handle_events()
-        screen.fill(config.GREEN) # Use color from config
+        screen.fill(config.WHITE) # Use color from config
+        draw_text(screen, 'WELCOME!', 50, font_name, font_color, text_pos, bold=True)
+        draw_text(screen, 'YOU WIN!!', 70, font_name2, font_color2, text_pos2, italic=True )
+        draw_text(screen, 'OH NO!', 100, font_name3, font_color3, text_pos3, bold=True, italic=True)
+        
+        
+
         pygame.display.flip()
 
         # Limit the frame rate to the specified frames per second
